e434ea2e24e8022ce22863ea0a7fe144af82b38d
Fernando Melo <nandoapm@gmail.com>
2023-04-06 14:47:54 -0300

    feat: insert migration in project

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,48 @@
+/* eslint-disable prettier/prettier */
+
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  OneToMany,
+  UpdateDateColumn,
+} from 'typeorm';
+//import { AddressEntity } from '../../address/entities/address.entity';
+//import { OrderEntity } from '../../order/entities/order.entity';
+
+@Entity({ name: 'user' })
+export class UserEntity {
+  @PrimaryGeneratedColumn('rowid')
+  id: number;
+
+  @Column({ name: 'name', nullable: false })
+  name: string;
+
+  @Column({ name: 'email', nullable: false })
+  email: string;
+
+  @Column({ name: 'phone' })
+  phone: string;
+
+  @Column({ name: 'cpf', nullable: false })
+  cpf: string;
+
+  @Column({ name: 'password', nullable: false })
+  password: string;
+
+  @Column({ name: 'type_user', nullable: false })
+  typeUser: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  // @OneToMany(() => AddressEntity, (address) => address.user)
+  // addresses?: AddressEntity[];
+
+  // @OneToMany(() => OrderEntity, (order) => order.address)
+  // orders?: OrderEntity[];
+}
