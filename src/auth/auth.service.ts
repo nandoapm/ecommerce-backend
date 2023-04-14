bf021a648774889a65306dd954f8699218925a11
Fernando Melo <nandoapm@gmail.com>
2023-04-14 18:14:18 -0300

    feat: create unit test in auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-//import { validatePassword } from '../utils/password';
+import { validatePassword } from '../utils/password';
 import { ReturnUserDto } from '../user/dtos/returnUser.dto';
 import { UserEntity } from '../user/entities/user.entity';
 import { UserService } from '../user/user.service';
